14bdfb86a3d8356d119ff25b4b27696eddc89ad1
ong6 <junxiongong2@gmail.com>
2021-02-08 01:58:12 +0800

    Complete Lab3

---
+++ lab/lab3/sample3.asm
@@ -0,0 +1,19 @@
+# sample3.asm
+       .data 0x10000100
+msg:   .asciiz "Hello"
+       .text
+
+main:  li $v0, 4
+       la $a0, msg
+       syscall
+
+       lb $t0, 4($a0)
+       addi $t0, -32
+       sb $t0, 4($a0)
+       syscall
+                          # load 'o' into $t0
+                          # change $t0 to ASCII value of 'O'
+                          # store $t0 into the memory location of 'o'
+                          # perform another syscall
+       li $v0, 10
+       syscall
