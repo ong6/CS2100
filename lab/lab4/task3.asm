4db97c623c716f0d88c5f8923c8e59d1b01c31eb
ong6 <junxiongong2@gmail.com>
2021-03-01 13:39:42 +0800

    Finish lab4

--- lab/lab4/task3.asm
+++ lab/lab4/task3.asm
@@ -4,14 +4,26 @@ arrayA: .word 11, 0, 31, 22, 9, 17, 6, 9    # arrayA has 8 values
 count:  .word 0                         # dummy value
 
   .text
+
 main:
     # code to setup the variable mappings
     la $t0, arrayA    # load address of array
     la $s1, count     # load value of count
     lw $t8, 0($s1)
 
-    li $t1, 8         # length of array
-    li $t9, 0;        # length of counter
+    # for reading 
+    addi $t6, $0, 0
+    addi $t7, $0, 28
+
+read: 
+    li $v0, 5
+    syscall
+    add $s7, $0, $v0
+    add $s8, $t0, $t6
+    sw $s7, 0($s8)
+    addi $t6, $t6, 4
+    slt $s2, $t7, $t6
+    beq $s2, $0, read
 
     # code for reading in the user value X
     li   $v0, 5         # read user val
@@ -20,6 +32,8 @@ main:
 
     # code for counting multiples of X in arrayA
     addi $t5, $t2, -1         # store mask to check if multiple
+    addi $t1, $0, 8           # array size
+    addi $t9, $0, 0           # counter
 
 loop: 
     bge $t9, $t1, end_loop    # loop till over the size of array
