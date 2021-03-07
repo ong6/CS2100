ac6cfb6b447e708abe7efd00fea1279e26231b92
ong6 <junxiongong2@gmail.com>
2021-03-08 02:23:04 +0800

    Finish Task 2: Part 1 & Part 2

--- lab/lab5/arrayFunction.asm
+++ lab/lab5/arrayFunction.asm
@@ -8,6 +8,9 @@ main:
 	# Print the original content of array
 	# setup the parameter(s)
 	# call the printArray function
+	la $a0, array
+	li $a1, 10
+	jal printArray
 
 	# Ask the user for two indices
 	li   $v0, 5         	# System call code for read_int
@@ -22,6 +25,10 @@ main:
 	# setup the parameter(s)
 	# call the function
 
+	add $a0, $t0, $zero
+	add $a1, $t1, $zero
+	la $t1, array
+	jal findMin
 
 	# Print the min item
 	# place the min item in $t3	for printing
@@ -35,7 +42,7 @@ main:
     la   $a0, newl    	# 
     syscall       		# print newline
 
-	#Calculate and print the index of min item
+	# Calculate and print the index of min item
 	
 	# Place the min index in $t3 for printing	
 
@@ -89,4 +96,32 @@ e1:
 #Registers used: <Fill in with your register usage>
 #Assumption: Array element is word size (4-byte), $a0 <= $a1
 findMin:
+
+	# first item in the array
+	sll $t2, $a0, 2 	# Multiply lower array item by 4
+	add $t2, $t2, $t1 	# pointing at lower array item
+	lw $t3, 0($t2) 		# $t3 is the load lower array item
+
+l2:
+	beq $a0, $a1, e2
+
+	sll $t5, $a0, 2		# Multiply curr item by 4
+	add $t5, $t5, $t1	# point at current item
+	lw $t4, 0($t5)		# value of curr item
+
+	addi $a0, $a0, 1
+
+	bgt $t3, $t4, setlow # check if t4 is lower than t3
+
+	j l2
+
+setlow: 
+	add $t3, $t4, $zero 	# set t3 as the lower value
+	j l2
+
+e2: 
 	jr $ra			# return from this function
+
+	; sll $t3, $a0, 2 	# Multiply higher array item by 4
+	; add $t3, $t3, $t1 	# pointing at higher array item
+	; lw $t3, 0($t3) 		# $t2 is the load higher array item
